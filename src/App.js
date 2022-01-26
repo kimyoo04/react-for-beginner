dffaea642882f17c044293078cb68c3ba6f93367
kimyoo <kimyoo04eco@naver.com>
2022-01-27 08:50:45 +0900

    git start

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from "react";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>} />
+        <Route path="/movie/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
